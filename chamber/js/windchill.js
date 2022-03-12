67000723d8211fa81c814b8135b25cd39f70c11e
IsaiasZc <78005126+IsaiasZc@users.noreply.github.com>
2022-03-12 18:01:06 -0500

    weather Json

--- chamber/js/windchill.js
+++ chamber/js/windchill.js
@@ -1,11 +1,32 @@
-let temperature =parseInt(document.getElementById('temperature').innerHTML);
-let windSpeed = parseFloat(document.getElementById('wind-speed').innerHTML);
+const apiURL = "http://api.openweathermap.org/data/2.5/weather?&q=Lima&units=Imperial&appid=31b886c84c9c9db98eeba6960d675bee";
+let temperature,windSpeed;
+
+fetch(apiURL)
+    .then((responde) => responde.json())
+    .then((jsonObject) => {
+        // console.log(jsonObject)
+        const iconSrc = `https://openweathermap.org/img/w/${jsonObject.weather[0].icon}.png`;
+        const desc = jsonObject.weather[0].description;
+        document.querySelector("#weather-description").textContent = desc;
+        document.querySelector("#temperature").textContent = jsonObject.main.temp;
+        temperature = jsonObject.main.temp;
+        windSpeed = jsonObject.wind.speed;
+        document.querySelector("#wind-speed").textContent = windSpeed;
+        document.querySelector("#weather-icon").setAttribute("src", iconSrc);
+        document.querySelector("#weather-icon").setAttribute("alt", desc);
+    })
+
+// -------------- Calculate the Wind Chill -----------------
+
+
+// let temperature =parseInt(document.getElementById('temperature').innerHTML);
+// let windSpeed = parseFloat(document.getElementById('wind-speed').innerHTML);
 
 // let calculateWindChill = 35.74 + (0.6215 * temperature) - (35.75 * (windSpeed ** 0.16)) + (.4275 * temperature * (windSpeed ** .16))
 
 const calculateWindChill = (t, s) => {
     if( t <= 50 && s >3){
-    let windChill = 35.74 + (0.6215 * t) - (35.75 * (s ** 0.16)) + (.4275 * t * (s ** .16))
+    let windChill = 35.74 + (0.6215 * t) - (35.75 * (s ** 0.16)) + (.4275 * t * (s ** .16));
     return windChill.toFixed(2)
     } else {
         return  "N/A"
---
+++ lesson-10/js/weatherapi.js
@@ -0,0 +1,13 @@
+const apiURL = `http://api.openweathermap.org/data/2.5/weather?&q=Fairbanks&appid=31b886c84c9c9db98eeba6960d675bee`;
+
+fetch(apiURL)
+    .then((response) => response.json())
+    .then((jsonObject) => {
+        const iconSrc = `https://openweathermap.org/img/w/${jsonObject.weather[0].icon}.png`;
+        const desc = jsonObject.weather[0].description;
+        document.querySelector("#current-temp").textContent = jsonObject.main.temp;
+        document.querySelector("#icon-src").textContent = iconSrc;
+        document.querySelector("#weathericon").setAttribute("src", iconSrc);
+        document.querySelector("#weathericon").setAttribute("alt", desc);
+        document.querySelector("figcaption").textContent = desc;
+    });
